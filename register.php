2861b6566c43d85242ecf773bd98f738007dd48a
xReyyz <rlb@njit.edu>
2025-10-15 17:09:21 -0400

    updated register

--- register.php
+++ register.php
@@ -7,37 +7,57 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('10.147.17.197', 5672, 'rey', 'rey', 'projectVhost');
 $channel = $connection->channel();
 
+$channel->queue_declare('auth_request', false, true, false, false);
+
 list($callbackQueue,,) = $channel->queue_declare("", false, false, true, false);
 
 $username = $_POST['uname'] ?? '';
 $password = $_POST['pword'] ?? '';
 $email    = $_POST['email'] ?? '';
 
-$data = ["action" => "register", "username" => $username, "password" => $password, "email" => $email];
+if (!$username || !$password || !$email) {
+    echo json_encode(["status" => "error", "message" => "All fields required"]);
+    exit;
+}
+
+$data = [
+    "action"   => "register",
+    "username" => $username,
+    "password" => $password,
+    "email"    => $email
+];
 
 $corrId = uniqid();
-$msg = new AMQPMessage(json_encode($data), ['correlation_id' => $corrId, 'reply_to' => $callbackQueue]);
+
+$msg = new AMQPMessage(
+    json_encode($data),
+    [
+        'correlation_id' => $corrId,
+        'reply_to' => $callbackQueue
+    ]
+);
 
 $channel->basic_publish($msg, '', 'auth_request');
 
 $response = null;
-$channel->basic_consume($callbackQueue, '', false, true, false, false, function($msg) use (&$response, $corrId) {
+$callback = function($msg) use (&$response, $corrId) {
     if ($msg->get('correlation_id') === $corrId) {
         $response = $msg->body;
     }
-});
+};
+$channel->basic_consume($callbackQueue, '', false, true, false, false, $callback);
 
 $start = time();
 while ($response === null && (time() - $start) < 5) {
     $channel->wait(null, false, 1);
 }
-
 if ($response === null) {
     $response = json_encode(["status" => "error", "message" => "No response from DB"]);
 }
-
 header("Content-Type: application/json");
 echo $response;
 
-$channel->close
+$channel->close();
+$connection->close();
 ?>
+
