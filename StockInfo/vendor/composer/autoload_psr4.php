f404824d0c3b8e3512133d0583d02e09807ead43
Benjamin <benjaminbrianpang@gmail.com>
2025-10-27 21:35:37 +0000

    FINAL FINAL Midterm Commit (last one did not remove test files)

--- StockInfo/vendor/composer/autoload_psr4.php
+++ StockInfo/vendor/composer/autoload_psr4.php
@@ -11,6 +11,7 @@ return array(
     'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
     'PhpAmqpLib\\' => array($vendorDir . '/php-amqplib/php-amqplib/PhpAmqpLib'),
     'ParagonIE\\ConstantTime\\' => array($vendorDir . '/paragonie/constant_time_encoding/src'),
+    'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
